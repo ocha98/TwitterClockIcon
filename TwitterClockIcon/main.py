8c3f28510ed1639f3c922800fa46453bf408b32b
ocha98 <48848793+ocha98@users.noreply.github.com>
2024-08-03 19:23:20 +0900

    [Fix]

--- TwitterClockIcon/main.py
+++ TwitterClockIcon/main.py
@@ -1,8 +1,9 @@
 import datetime
 import os
 from dotenv import load_dotenv
-from .twitter_api import UpdateProfileIcon
-from .util import time_rotate_image
+import tweepy
+from PIL import Image
+import datetime
 
 load_dotenv()
 
@@ -11,8 +12,23 @@ API_KEY_SECRET = os.environ['API_KEY_SECRET']
 ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
 ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
 
+def time_rotate_image(img_path,
+                      save_path,
+                      timezone,
+                      fillcolor = (0, 0, 0),
+                      ):
+    """
+    アイコンの頭部を時計の短針の位置へ回転した画像を生成する
+    画像はsave_pathの場所に保存される
+    回転角は1時間刻み
+    """
+    img = Image.open(img_path)
+    date = datetime.datetime.now(tz=timezone)
+    arg = date.hour%12 * 360//12
+    rotated_img = img.rotate(360 - arg, fillcolor=fillcolor)
+    rotated_img.save(save_path)
+
 def main():
-    api = UpdateProfileIcon(API_KEY, API_KEY_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
     timezone = datetime.timezone(datetime.timedelta(hours = 9))
     date = datetime.datetime.now(tz = timezone)
 
@@ -24,6 +40,13 @@ def main():
     else:
         time_rotate_image("img/normal.png", save_path, timezone, fillcolor = fillcolor)
 
-    api.update_profile_icon(save_path)
+    auth = tweepy.OAuth1UserHandler(
+        API_KEY,
+        API_KEY_SECRET,
+        ACCESS_TOKEN,
+        ACCESS_TOKEN_SECRET,
+        )
+    api = tweepy.API(auth)
+    api.update_profile_image(save_path)
 
     os.remove(save_path)
